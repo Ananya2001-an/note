3f8c0ffd59122d67b005a13e5a7e5f5dba104000
Ananya Nayak <55504616+Ananya2001-an@users.noreply.github.com>
2023-02-11 06:30:13 +0000

    feat: connecting server

--- src/Components/FilePondUpload.js
+++ src/Components/FilePondUpload.js
@@ -10,22 +10,17 @@ import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 function FilePondUpload() {
-  const [files, setFiles] = useState([]);
+  const [file, setFile] = useState([]);
 
   return (
     <div className="container mt-4">
       <p className="mb-3 text-small muted" style={{fontFamily:"Roboto Mono, monospace", color:"var(--font-color)"}}>**Below you will get a preview of the attached file**</p>
       <FilePond
-        acceptedFileTypes: ['image/*'],
+        acceptedFileTypes={"image/*"}
         name="img"
         allowImagePreview={true}
-        allowMultiple={true}
-        files={files}
-        onupdatefiles={setFiles}
-        maxFiles={5}
-        instantUpload={true}
-        allowReorder={true}
-        server="/" // File upload api goes here
+        files={file}
+        onupdatefiles={setFile}
       />
     </div>
   );
--- src/Components/Notes/NewNote.js
+++ src/Components/Notes/NewNote.js
@@ -1,19 +1,55 @@
-import React from 'react'
-import FormFieldsNote from './FormFieldsNote'
+import React, {useRef} from 'react'
+import axios from 'axios'
+import FilePondUpload from "../FilePondUpload.js";
 
 export default function NewNote() {
+const nameRef = useRef();
+const noteRef = useRef();
+const imgRef = useRef();
+    
+const addNote = (e)=>{
+    e.preventDefault();
+    console.log(nameRef.current.value)
+    // const note = {
+    //   name: nameRef.current.value,
+    //   note: noteRef.current.value,
+    //   img: URL.createObjectURL(imgRef.files[0])
+    // };
+    // axios.post(`${process.env.REACT_APP_SERVER_URL}/notes`, note)
+    // .then(res => console.log(res))
+  }
+
   return (
     <div className="container">
-    <h2 className="page-header">New Note</h2>
-    <div className="inner-container">
-    <form action="/notes" method="POST">
-        <FormFieldsNote/>
-        <div className="btn-row">
-            <a className="btn btn-primary" href="/notes">Cancel</a>
-            <button className="btn btn-primary" type="submit">Create</button>
-        </div>
-    </form>
+      <h2 className="page-header">New Note</h2>
+      <div className="inner-container">
+        <form onSubmit={(e) => addNote(e)}>
+          <div className="form-row">
+            <div className="form-item">
+              <label>Name:</label>
+              <input type="text" ref={nameRef} />
+            </div>
+            <div className="form-item">
+              <label>Cover Image:</label>
+              <FilePondUpload ref={imgRef} />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-item">
+              <label>Note:</label>
+              <textarea style={{ fontSize: "20px" }} ref={noteRef} />
+            </div>
+          </div>
+          <div className="btn-row">
+            <a className="btn btn-primary" href="/notes">
+              Cancel
+            </a>
+            <button className="btn btn-primary" type="submit">
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
