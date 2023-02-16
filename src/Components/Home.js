10a29b44cead18b85c45db0882c975dd56bc767c
Mareedu Saibabu <mareedu.saibabu@publicissapient.com>
2023-02-16 20:36:54 +0530

    Loading Effect Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,43 +2,55 @@ import React, { useState, useEffect } from "react";
 import { FaFile } from "react-icons/fa";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Loading from './Loading';
 
 export default function Home() {
   const [assignments, setAssignments] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_SERVER_URL}`)
       .then((res) => setAssignments(res.data));
+    setLoading(false);
   }, []);
 
   return (
-    <div className="container">
-      <h1 className="page-header">
-        Important Assignments <FaFile />
-      </h1>
-      <div className="inner-container">
-        <div className="grid">
-          {assignments.length !== 0 &&
-            assignments.map((assign) => {
-              return (
-                <>
-                  <p
-                    style={{ textDecoration: "underline", cursor: "pointer" }}
-                    onClick={() =>
-                      navigate("/assignments/view", { state: { assign } })
-                    }
-                  >
-                    {assign.name}
-                  </p>
-                  <p>{assign.subject}</p>
-                  <p>{assign.deadline.split("T")[0]}</p>
-                </>
-              );
-            })}
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className='container'>
+          <h1 className='page-header'>
+            Important Assignments <FaFile />
+          </h1>
+          <div className='inner-container'>
+            <div className='grid'>
+              {assignments.length !== 0 &&
+                assignments.map((assign) => {
+                  return (
+                    <>
+                      <p
+                        style={{
+                          textDecoration: 'underline',
+                          cursor: 'pointer',
+                        }}
+                        onClick={() =>
+                          navigate('/assignments/view', { state: { assign } })
+                        }
+                      >
+                        {assign.name}
+                      </p>
+                      <p>{assign.subject}</p>
+                      <p>{assign.deadline.split('T')[0]}</p>
+                    </>
+                  );
+                })}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
---
+++ src/Components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CSS/loading.css';
+
+const Loading = () => {
+  return (
+    <div className='loading-container'>
+      <div className='loading'></div>
+    </div>
+  );
+};
+
+export default Loading;
